e02df69ddb495dbdf51b425cc5fa15edd5f92669
noolands <142060008+noolands@users.noreply.github.com>
2023-08-14 18:57:37 +0300

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-let h1Id=document.getElementById('h1');
-let buttonId=document.getElementById('button');
-let counter=0;
-buttonId.addEventListener('click',function(){
-	counter++;
-	h1Id.textContent='score: '+counter;
-});
